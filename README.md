b911333724d8ab6ad0098c510ab18679d3acfb69
Daniel Kogan <danielkoganx@gmail.com>
2021-12-31 15:20:11 -0500

    autosave

---
+++ README.md
@@ -0,0 +1,9 @@
+# Kogan-Atlas
+
+Atlas Blockchain Network
+
+# Features
+
+Able to make post requests to the network and create blocks
+
+Linked to Firebase database as well as local JSON file
--- request.py
+++ request.py
@@ -1,8 +1,15 @@
 import requests,json
+import base64
 
 url = 'http://192.168.0.102:5000/chain'
 
-post_data = json.dumps({'data': 'i just made the 3rd post request to the blockchain'})
+#post_data = json.dumps({'data': 'post post post request'})
+
+#creating an NFT
+with open("t.png", "rb") as imageFile:
+    imagestr = base64.b64encode(imageFile.read())
+
+
 headers = {"Content-Type": 'application/json', 'username': 'daniel'}
 
 post = requests.post(url, headers=headers, data=post_data)
